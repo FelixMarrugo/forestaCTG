02a46e2ea65428f0dc92e8544a8af85b5b242345
Felix Marrugo <felmacom@gmail.com>
2024-10-22 02:15:33 -0500

    Se añade el modulo de users y su crud. Ademas se agregan, entidades, dtos y validaciones haciendo uso de pipes, como class-validatos

---
+++ backend/src/users/constroller/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { UsersService } from './../service/users.service';
+import { CreateUserDto, UpdateUserDto } from './../dtos/users.dtos';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.userService.create(body);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.userService.update(id, payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(+id);
+  }
+}
---
+++ backend/src/users/dtos/users.dtos.ts
@@ -0,0 +1,26 @@
+import { IsString, IsNotEmpty, IsEmail, IsInt, IsUrl } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsInt()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  avatar: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
