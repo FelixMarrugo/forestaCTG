86a3544bbabe8aa899b79ef9876bf788946b7ab2
YJoel <joelgarcia6601@gmail.com>
2024-12-01 00:36:03 -0500

    Se elimino importación inutilizada

--- backend/src/inventary/controller/inventary.controller.ts
+++ backend/src/inventary/controller/inventary.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { InventaryService } from '../services/inventary.service';
 import { CreateTreeDto, UpdateTreeDto } from '../dtos/arbol.dto';
 
