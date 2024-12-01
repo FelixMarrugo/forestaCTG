93195e1f1bac0c55f36e63fc4c2dc4f77b2d4dbf
YJoel <joelgarcia6601@gmail.com>
2024-12-01 17:22:17 -0500

    Se agregó el decorador @Schema en el archivo user.entity e importación

--- backend/src/users/entites/users.entity.ts
+++ backend/src/users/entites/users.entity.ts
@@ -1,10 +1,12 @@
-import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export class User {
+@Schema()
+export class User extends Document {
   @Prop({ required: true })
   id: string;
 
-  @Prop()
+  @Prop({ required: true })
   username: string;
 
   @Prop({ required: true })
