bf956d0e88d65eda251c210a88c88385dc10ac57
YJoel <joelgarcia6601@gmail.com>
2024-12-14 00:36:56 -0500

    Se agregó el endpoint de filtrado para los árboles

--- backend/src/inventary/controller/inventary.controller.ts
+++ backend/src/inventary/controller/inventary.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { InventaryService } from '../services/inventary.service';
 import { CreateTreeDto, UpdateTreeDto } from '../dtos/tree.dto';
 
@@ -8,7 +8,12 @@ export class InventaryController {
 
   @Get()
   getAll() {
-    return this.inventaryServices.getAll();
+    return this.inventaryServices.getAll({});
+  }
+
+  @Post('filter/')
+  getTreesWithFilter(@Body() filter) {
+    return this.inventaryServices.getAll(filter);
   }
 
   @Get(':id')
